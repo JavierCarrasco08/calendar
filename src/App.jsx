31466e5f7427372692ad1609343baf6cc00d3f52
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-11 16:29:32 -0400

    Refactorizando y subiedo la informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-function App() {}
+import { useEffect, useState } from "react";
+import Hour from "./components/hour/Hour";
+
+function App() {
+  const [hour, setHour] = useState(new Date());
+
+  useEffect(() => {
+    let intervalId = setInterval(() => setHour(new Date()), 1000);
+
+    return () => clearInterval(intervalId);
+  });
+
+  return (
+    <main>
+      <header>
+        <Hour hour={hour.toLocaleTimeString()} />
+      </header>
+    </main>
+  );
+}
 
 export default App;
--- src/components/hour/Hour.jsx
+++ src/components/hour/Hour.jsx
@@ -1,11 +1,3 @@
-import { useState, useEffect } from "react";
-
-export default function Hour() {
-  const [hour, setHour] = useState(new Date());
-  useEffect(() => {
-    let intervalId = setInterval(() => setHour(new Date()), 1000);
-
-    return () => clearInterval(intervalId);
-  });
-  return <h2>{hour.toLocaleTimeString()}</h2>;
+export default function Hour({ hour }) {
+  return <h2>{hour}</h2>;
 }
